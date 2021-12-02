3f856d9cc1137492ac0554bab117f9c7c8bb2ac0
fruitvodka <varshav0119@gmail.com>
2021-12-02 23:25:22 +0530

    Class Add script

--- scripts/ips678/cleaning_users.py
+++ scripts/ips678/cleaning_users.py
@@ -1,8 +1,6 @@
 import pymongo
 import urllib
 import ssl
-import datetime
-import pytz
 
 MONGODB_URL = "mongodb+srv://admin:{password}@aryabota-db-cluster.kbxud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 MONGODB_PASSWORD = "stressbots@123"
@@ -12,11 +10,6 @@ MONGODB_PASSWORD = "stressbots@123"
 client = pymongo.MongoClient(MONGODB_URL.format(password = urllib.parse.quote_plus(MONGODB_PASSWORD)),ssl_cert_reqs=ssl.CERT_NONE)
 
 db = 'IPS678'
-
-utc=pytz.UTC
-
-current_time = datetime.datetime.now() - datetime.timedelta(days = 2)
-current_time = utc.localize(current_time)
 
 # Generating Cleaned User DB
 
@@ -35,6 +28,24 @@ for record in records:
                 all_users[record['email']] = record
             if record['email'] == '7519':
                 all_users[record['email']]['skills'] = record['skills']
+
+# cleaning field-divided                
+for email in all_users:
+    print(all_users[email])
+    if 'field-divided' in all_users[email]:
+        field_divided = all_users[email]["field-divided"]
+        if '8' in field_divided:
+            field_divided = '8'
+        elif '6' in field_divided:
+            field_divided = '6'
+        elif '7' in field_divided:
+            field_divided = '7'
+        else:
+            field_divided = 'unknown'
+    else:
+        field_divided = 'unknown'
+    all_users[email]["field-divided"] = field_divided
+
 for email in all_users:
     client['IPS678']['CleanedUser'].insert_one(all_users[email])
 
