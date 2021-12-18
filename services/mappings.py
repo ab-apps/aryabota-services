3d924522958e72977cf5b8332d0d7a2e1a33da2f
fruitvodka <varshav0119@gmail.com>
2021-12-18 18:07:31 +0530

    Adding Mappings file

---
+++ services/mappings.py
@@ -0,0 +1,48 @@
+class_mappings = {
+    "problem.problem_type": {
+        "class": "problem",
+        "attribute_name": "type"
+    },
+    "problem.statement": {
+        "class": "problem",
+        "attribute_name": "statement"
+    },
+    "problem.answer": {
+        "class": "problem",
+        "attribute_name": "answer"
+    },
+    "initial_state.grid.dimensions.row": {
+        "class": "grid",
+        "attribute.name": "rows"
+    },
+    "initial_state.grid.dimensions.column": {
+        "class": "grid",
+        "attribute.name": "columns"
+    },
+    "initial_state.grid.homes": {
+        "class": "grid",
+        "attribute.name": "homes",
+        "default": []
+    },
+    "initial_state.grid.obstacles": {
+        "class": "grid",
+        "attribute.name": "obstacles",
+        "default": []
+    },
+    "initial_state.aryabota.position.row": {
+        "class": "aryabota",
+        "attribute_name": "row"
+    },
+    "initial_state.aryabota.position.column": {
+        "class": "aryabota",
+        "attribute_name": "column"
+    },
+    "initial_state.aryabota.dir": {
+        "class": "aryabota",
+        "attribute_name": "dir"
+    },
+    "initial_state.aryabota.pen": {
+        "class": "aryabota",
+        "attribute_name": "pen"
+    }
+}
